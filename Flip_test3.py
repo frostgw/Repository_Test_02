dbb3ffdadeddd9b518b974930e448cc579fed150
frostgw <frostgw@users.noreply.github.com>
2016-11-07 22:38:46 -0500

    Add files via upload
    This is a Flip Test of Colors. Please try out and let me know if it works for you.

---
+++ Flip_test3.py
@@ -0,0 +1,17 @@
+#-----------------------------------------------------------------------
+#Written by FrostGW Version 1.0
+#This program will ask for a color, then see if you choose the correct color.
+#Start Program 
+import random
+color = (input('What is your color Red, Blue or Green: '))
+print('Hello this is a Test of Color')
+#Note range of numbers starting from 0. example 0,3 is 0 and 1 and 3.
+if random.randrange(0,3)== 0:
+    print('Red is it')
+elif random.randrange(0,3)== 1:
+    print('Blue is it')
+else:
+	print('Green is it')
+print('Thank you for playing. ')
+#End Program    
+#-----------------------------------------------------------------------
